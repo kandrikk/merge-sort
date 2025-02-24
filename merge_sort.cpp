080e58a6e6a0dce04f2d3870438b90cd87b91d96
kandrikk <nkondrakov26@gmail.com>
2025-02-24 20:49:37 +0300

    mergesort + merge

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,21 +1,67 @@
 #include <iostream>
 #include <algorithm>
 
+void merge(int *src, int *dst, int left, int mid, int right)
+{
+    int i = left;
+    int j = mid;
+    int k = left;
+    while (i < mid && j < right)
+    {
+        if (src[i] < src[j])
+        {
+            dst[k] = src[i];
+            i += 1;
+            k += 1;
+        } else {
+            dst[k] = src[j];
+            j += 1;   
+            k += 1;
+        }
+    }
+
+    while (i < mid)
+    {
+        dst[k] = src[i];
+        i += 1;
+        k += 1;
+    }
+
+    while (j < right)
+    {
+        dst[k] = src[j];
+        j += 1;
+        k += 1;
+    }
+}
+
 void merge_sort(int *arr, int n)
 {
     int *brr = new int [n];
-    int src = arr;
-    int dst = brr;
+    int *src = arr;
+    int *dst = brr;
     int w = 1;
-    int i = 0;
 
     while (w < n)
     {
+        int i = 0;
+        
         while (i < n)
         {
-            meg
+            merge(src, dst, i , std::min(i+w, n), std::min(i + 2*w, n));
+            i = i + 2*w;
         }
+
+        w = 2*w;
+        std::swap(src, dst);
     }
+
+    if (src != arr)
+    {
+        std::copy(src,src + n, arr);
+    }
+    
+    delete[] brr;
 }
 
 int main()
